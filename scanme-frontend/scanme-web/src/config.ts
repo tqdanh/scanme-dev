cf8f9a0b5ed816fe4e1a4ae1cc087ea85a3153ce
Danh Tran <tqdanh@tma.com.vn>
2022-05-18 15:06:39 +0700

    update FE config

--- scanme-frontend/scanme-web/src/config.ts
+++ scanme-frontend/scanme-web/src/config.ts
@@ -1,13 +1,13 @@
 const localConfigUrl = {
-  authenticationServiceUrl: 'http://103.199.6.149:8080',
-  supplyChainServiceUrl: 'http://103.199.6.149:8080',
-  organizationServiceUrl: 'http://103.199.6.149:8080',
-  qrcodeBaseUrl: 'http://103.199.6.149:8088', // local network
+  authenticationServiceUrl: 'http://localhost:8080',
+  supplyChainServiceUrl: 'http://localhost:8080',
+  organizationServiceUrl: 'http://localhost:8080',
+  qrcodeBaseUrl: 'http://localhost:8088', // local network
   reverseGeocodingService: 'https://us1.locationiq.com/v1/reverse.php',
-  productServiceUrl: 'http://103.199.6.149:8080',
-  imageURL: 'http://103.199.6.149:8080',
-  itemServiceUrl: 'http://103.199.6.149:8080',
-  providerServiceUrl: 'http://103.199.6.149:8080',
+  productServiceUrl: 'http://localhost:8080',
+  imageURL: 'http://localhost:8080',
+  itemServiceUrl: 'http://localhost:8080',
+  providerServiceUrl: 'http://localhost:8080',
   geoCodingService: 'https://api.mapbox.com/geocoding/v5/mapbox.places'
 };
 const config = localConfigUrl;
